140dc5cbc4d0b26ff6b5c5d025344043664ea5cb
Kusu <cyli22@m.fudan.edu.cn>
2026-03-02 22:14:42 +0800

    test: update dependencies and register new plugins for Windows

--- lib/pages/main/controller.dart
+++ lib/pages/main/controller.dart
@@ -1,8 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class MainController extends GetxController {
+  late final PageController pageController;
+  final RxInt selectedIndex = 0.obs;
+
   @override
   void onInit() {
     super.onInit();
+    pageController = PageController(initialPage: selectedIndex.value);
+  }
+
+  void setIndex(int index) {
+    if (selectedIndex.value != index) {
+      selectedIndex.value = index;
+      pageController.jumpToPage(index);
+    }
+  }
+
+  @override
+  void onClose() {
+    pageController.dispose();
+    super.onClose();
   }
 }
--- lib/pages/main/view.dart
+++ lib/pages/main/view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../library/view.dart';
+import '../shelf/view.dart';
+import '../discover/view.dart';
+import '../setting/view.dart';
 
 import 'controller.dart';
 
@@ -10,8 +14,41 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final controller = Get.putOrFind(MainController.new);
     return Scaffold(
-      appBar: AppBar(title: const Text('Main')),
-      body: const Center(child: Text('Main Page')),
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: controller.pageController,
+        children: const [
+          LibraryPage(),
+          ShelfPage(),
+          DiscoverPage(),
+          SettingPage(),
+        ],
+      ),
+      bottomNavigationBar: Obx(
+        () => BottomNavigationBar(
+          currentIndex: controller.selectedIndex.value,
+          onTap: controller.setIndex,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.library_books),
+              label: 'Library',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.book),
+              label: 'Shelf',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.explore),
+              label: 'Discover',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
